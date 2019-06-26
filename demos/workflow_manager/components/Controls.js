e663a300284d976317546000a78e70e25a837b74
vrancurel <vianney.rancurel@gmail.com>
2019-06-26 09:30:36 -0700

    no workflow name for now

--- demos/workflow_manager/components/Controls.js
+++ demos/workflow_manager/components/Controls.js
@@ -6,53 +6,12 @@ import { diagramEngine } from './Engine';
 export class Controls extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      showFileName: false,
-      fileName: undefined
-    };
 
     this.newWorkflow = this.newWorkflow.bind(this);
     this.loadWorkflow = this.loadWorkflow.bind(this);
     this.saveWorkflow = this.saveWorkflow.bind(this);
     this.checkWorkflow = this.checkWorkflow.bind(this);
     this.uploadWorkflow = this.uploadWorkflow.bind(this);
-    this.inputChanged = this.inputChanged.bind(this);
-    this.setFileName = this.setFileName.bind(this);
-    this.cancelFileName = this.cancelFileName.bind(this);
-    this.okFileName = this.okFileName.bind(this);
-  }
-
-  inputChanged(e) {
-    e.preventDefault();
-    this.setState({
-      fileName: e.target.value,
-    });
-  }
-
-  cancelFileName(event) {
-    event.stopPropagation();
-    event.preventDefault();
-    this.setState({
-      showFileName: false,
-      fileName: undefined
-    });
-  }
-
-  okFileName(event) {
-    const { model } = this.props;
-    event.stopPropagation();
-    event.preventDefault();
-    this.setState({
-      showFileName: false
-    });
-  }
-
-  setFileName(event) {
-    event.stopPropagation();
-    event.preventDefault();
-    this.setState({
-      showFileName: true
-    });
   }
 
   effectivelyLoadWorkflow(newModel) {
@@ -63,9 +22,6 @@ export class Controls extends React.Component {
     event.stopPropagation();
     event.preventDefault();
     const wed = new WorkflowEngineDefs();
-    this.setState({
-      fileName: undefined,
-    });
     const newModel = wed.generateNewModel();
     this.props.updateModel(newModel, { selectedNode: null });
   }
@@ -84,15 +40,11 @@ export class Controls extends React.Component {
   
   saveWorkflow(event) {
     const { model } = this.props;
-    if (this.state.fileName === undefined) {
-      window.confirm('missing file name');
-      return ;
-    }
     event.stopPropagation();
     event.preventDefault();
     const contentType = 'application/json;charset=utf-8;';
     const a = document.createElement('a');
-    a.download = this.state.fileName;
+    a.download = 'workflow.json';
     const modelEncoded = encodeURIComponent(JSON.stringify(model));
     a.href = `data:${contentType},${modelEncoded}`;
     a.target = '_blank';
@@ -114,10 +66,6 @@ export class Controls extends React.Component {
   uploadWorkflow() {
     const { model } = this.props;
     const wed = new WorkflowEngineDefs(model);
-    if (this.state.fileName === undefined) {
-      window.confirm('set a file name to workflow');
-      return ;
-    }
     try {
       wed.checkModel();
     } catch (err) {
@@ -125,7 +73,7 @@ export class Controls extends React.Component {
       return ;
     }
     const content = JSON.stringify(model);
-    const filename = this.state.fileName;
+    const filename = 'workflow.json';
     const form = new FormData();
     form.append('data', new File([new Blob([content])], filename));
     
@@ -145,30 +93,6 @@ export class Controls extends React.Component {
 
     const content = selectedNode ?
       JSON.stringify(selectedNode.serialize(), null, 2) : '';
-
-    const showFileName = () => {
-      if (this.state.showFileName) {
-        return <div>
-          <div>
-            <input className='workflow-name'
-              type="text"
-              name="name"
-              defaultValue={this.state.fileName}
-              onChange={this.inputChanged}/>
-          </div>
-          <div className='workflow-name-button'>
-            <button
-              onClick={this.cancelFileName}>
-              Cancel
-            </button>
-            <button
-              onClick={this.okFileName}>
-              Ok
-            </button>
-          </div>
-        </div>;
-      }
-    };
 
     return (
       <div className='controls'>
@@ -220,12 +144,6 @@ export class Controls extends React.Component {
                 onClick={ this.checkWorkflow }>
                 <span className="fa fa-check"/>Check</button>
             </div>
-            <div>
-              <button className="m-1 btn btn-w btn-primary"
-                onClick={this.setFileName}>
-                <span className="fa fa-chevron-right"/>Set File Name</button>
-            </div>
-            { showFileName() }
             <div>
               <button className="m-1 btn btn-w btn-primary"
                 onClick={ this.uploadWorkflow }>
